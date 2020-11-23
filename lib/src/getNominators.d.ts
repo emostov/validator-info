cd209ed9aa671dbd430a88cad6252079e98d1b51
emostov <32168567+emostov@users.noreply.github.com>
2020-11-22 19:49:08 -0800

    build: Remove lib

--- lib/src/getNominators.d.ts
+++
@@ -1,8 +0,0 @@
-import { ApiPromise } from '@polkadot/api';
-/**
- * Retrieve a list of all accounts that have the given `validatorId` as a nomination target.
- *
- * @param api ApiPromise connected to a node in the relevant network
- * @param validatorId ID of the validator to retrieve the nominators of
- */
-export declare function getNominators(api: ApiPromise, validatorId: string): Promise<any>;
